df14d3be9d4e2fe735c13d216710bce642e2959b
Grace Mavity <mavige01@students.ipfw.edu>
2016-10-13 22:59:24 -0400

    Skeleton for homepage and signup page. CSS skeleton and Javascript for validating the signup page.

---
+++ Javascript/main.js
@@ -0,0 +1,48 @@
+function validateSignup()
+			
+{
+				
+if (document.forms.signup.name.value == "")
+				
+{
+					
+alert("You must provide your name!");
+					
+return false;
+				
+}
+				
+else if (!document.forms.signup.email.value.match(/.+@ipfw.edu$/) && !document.forms.signup.email.value.match(/.+@students.ipfw.edu$/) )
+				
+{
+					
+alert("You must provide an IPFW email adddress!");
+					
+return false;
+			
+	}
+				
+else if (document.forms.signup.password1.value == "")
+				
+{
+					
+alert("You must provide a password!");
+					
+return false;					
+				
+}
+				
+else if (document.forms.signup.password1.value != document.forms.signup.password2.value)
+				
+{
+					
+alert("You must provide the same password twice!");
+					
+return false;
+				
+}
+			
+				
+return true;
+			
+}
