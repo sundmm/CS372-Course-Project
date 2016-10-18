174af91e955be81726f7876645e877c5144b2f7c
Mikah Sunderman <mmsunderman@yahoo.com>
2016-10-18 17:08:37 -0400

    Add forum files

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,48 +1,64 @@
 function validateSignup()
-			
+
 {
-				
-if (document.forms.signup.name.value == "")
-				
-{
-					
-alert("You must provide your name!");
-					
-return false;
-				
-}
-				
-else if (!document.forms.signup.email.value.match(/.+@ipfw.edu$/) && !document.forms.signup.email.value.match(/.+@students.ipfw.edu$/) )
-				
-{
-					
-alert("You must provide an IPFW email adddress!");
-					
-return false;
-			
+
+	if (document.forms.signup.name.value == "")
+
+	{
+
+		alert("You must provide your name!");
+
+		return false;
+
 	}
-				
-else if (document.forms.signup.password1.value == "")
-				
-{
-					
-alert("You must provide a password!");
-					
-return false;					
-				
+
+	else if (!document.forms.signup.email.value.match(/.+@ipfw.edu$/) && !document.forms.signup.email.value.match(/.+@students.ipfw.edu$/) )
+
+	{
+
+		alert("You must provide an IPFW email adddress!");
+
+		return false;
+
+	}
+
+	else if (document.forms.signup.password1.value == "")
+
+	{
+
+		alert("You must provide a password!");
+
+		return false;					
+
+	}
+
+	else if (document.forms.signup.password1.value != document.forms.signup.password2.value)
+
+	{
+
+		alert("You must provide the same password twice!");
+
+		return false;
+
+	}
+
+
+	return true;
+
 }
-				
-else if (document.forms.signup.password1.value != document.forms.signup.password2.value)
-				
+
+function validateCreateThread()
+
 {
-					
-alert("You must provide the same password twice!");
-					
-return false;
-				
-}
-			
-				
-return true;
-			
+	
+	if (document.forms.createthread.thread_subject.value == "")
+		
+	{
+		
+		alert("You must provide a thread subject.");
+		
+		return false;
+		
+	}
+
 }
